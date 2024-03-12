adab4296cc2b102fe5b5e9385b404d79b6b5c1be
bdg <bdg>
2024-03-12 09:14:07 +0800

    init

---
+++ c++/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 10;
+
+  // for (;;)
+  {
+    std::cout << i << endl;
+    std::cout << "hello" << std::endl;
+    std::cout << i << endl;
+  }
+
+  return 0;
+}
